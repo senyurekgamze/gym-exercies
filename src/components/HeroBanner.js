1ac996836ed016659652f689bc1f8e1d9a2a388a
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-04-20 14:24:20 +0200

    start to working Home page

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+function HeroBanner() {
+  return (
+    <Box>
+      <Typography color="#FF2625" fontSize="26px" fontWeight="600">
+        Fitness Clup
+      </Typography>
+    </Box>
+  );
+}
+
+export default HeroBanner;
